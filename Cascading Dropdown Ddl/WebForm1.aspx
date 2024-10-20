bbc63a940e6e8f48e0a4c0f77c84318c21319d40
ADMIN <ADMIN@ITD-LIS-WS-040>
2024-10-21 00:12:12 +0530

    Add project files.

---
+++ Cascading Dropdown Ddl/WebForm1.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Cascading_Dropdown_Ddl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                <table>
+                    <tr>
+                        <td>Country:</td>
+                        <td><asp:DropDownList runat="server" ID="ddlCountry" AutoPostBack="true" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged"></asp:DropDownList></td>
+                    </tr>
+                    <tr>
+                        <td>State:</td>
+                        <td><asp:DropDownList runat="server" ID="ddlState"></asp:DropDownList></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><asp:Button runat="server" ID="btnSubmit" Text="Submit" OnClick="btnSubmit_Click" /></td>
+                    </tr>
+                </table>
+            </center>
+        </div>
+    
+        <asp:GridView runat="server" ID="gv" OnRowCommand="gv_RowCommand" AutoGenerateColumns="false" Width="100%" style="text-align:center" >
+        <Columns>
+            <asp:TemplateField HeaderText="Id">
+                <ItemTemplate>
+                    <%#Eval("id") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Selected Country">
+                <ItemTemplate>
+                    <%#Eval("c_country") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Selected State">
+                <ItemTemplate>
+                    <%#Eval("s_name") %>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button runat="server" id="btnEdit" CommandName="btnedit" CommandArgument='<%#Eval("id") %>' Text="Edit" Width="100%"/>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button runat="server" id="btnDelete" CommandName="btndelete" CommandArgument='<%#Eval("id") %>' Text="Delete" Width="100%"/>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        </asp:GridView>
+        </form>
+</body>
+</html>
---
+++ Cascading Dropdown Ddl/WebForm1.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace Cascading_Dropdown_Ddl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source=ITD-LIS-WS-040\\SQLEXPRESS; initial catalog=dropdown; integrated security=true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                display();
+                displaycountry();
+                ddlState.Items.Insert(0, new ListItem("--Select--", "0"));
+                ddlState.Enabled = false;
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(btnSubmit.Text == "Submit")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("dbinsert", con);
+                cmd.Parameters.AddWithValue("@country", ddlCountry.SelectedValue);
+                cmd.Parameters.AddWithValue("@city", ddlState.SelectedValue);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                display();
+                clear();
+            }
+
+
+            if(btnSubmit.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("dbupdate", con);
+                cmd.Parameters.AddWithValue("@id", ViewState["id"]);
+                cmd.Parameters.AddWithValue("@country", ddlCountry.SelectedValue);
+                cmd.Parameters.AddWithValue("@city", ddlState.SelectedValue);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                display();
+                clear();
+            }
+            
+
+        }
+
+        public void displaycountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("displaycountry",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            ddlCountry.DataValueField = "c_id";
+            ddlCountry.DataTextField = "c_country";
+            ddlCountry.DataSource = dt;
+            ddlCountry.DataBind();
+            ddlCountry.Items.Insert(0, new ListItem("--Select--", "0"));
+            ddlState.Enabled = true;
+
+        }
+        public void displaystate()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("displaystate", con);
+            cmd.Parameters.AddWithValue("@c_id", ddlCountry.SelectedValue);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            ddlState.DataValueField = "s_id";
+            ddlState.DataTextField = "s_name";
+            ddlState.DataSource = dt;
+            ddlState.DataBind();
+            ddlState.Enabled = true;
+        }
+
+        public void display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("ddljoin", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            gv.DataSource = dt;
+            gv.DataBind();
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            displaystate();
+        }
+
+        protected void gv_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "btnedit")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("ddledit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                con.Close();
+                ddlCountry.SelectedValue = dt.Rows[0][1].ToString();
+                displaystate();
+                ddlState.SelectedValue = dt.Rows[0][2].ToString();
+                btnSubmit.Text = "Update";
+                ViewState["id"] = e.CommandArgument;
+
+            }
+
+            if(e.CommandName == "btndelete")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("ddldelete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();    
+            }
+        }
+
+        public void clear()
+        {
+           ddlCountry.SelectedValue = "0";            
+           ddlState.Items.Insert(0, new ListItem("--Select--", "0"));
+           ddlState.Enabled = false;
+        }
+    }
+}
